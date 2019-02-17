f398d3e92c2e8d887e7482029c8f65004d2ffd2f
ZacharyDavidSaunders <zacharydavidsaunders@gmail.com>
2019-02-17 00:20:55 -0800

    deploy to heroku

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var express = require('express');
 var VERSION = '1.0';
 var DOMAIN = "pseudoname.io";
 var NAME = "PseudonameAPI";
-var PORT = 5000;
-var forwardMxApiKey = process.env.forwardMxApiKey;
+var PORT = process.env.PORT || 5000;
+var forwardMxApiKey = process.env.herokuForwardMXApiKey || process.env.localForwardMxApiKey;
 
 //<-----------END OF DEFINITIONS----------->
 
@@ -27,30 +27,8 @@ app.use(require('./routes.js'));
 app.listen(PORT, () => {
     // Confirms successful server start.
     console.log('\x1b[7m',NAME+'(v'+VERSION+') has started.','\x1b[0m');
-    if(!dependencyCheck()) {
-        console.log('\x1b[37m\x1b[41m\x1b[1m','Dependency check failed.','\x1b[0m');
-        console.log('Exiting process.');
-        process.exit(1);
-    }
-    console.log('\x1b[42m\x1b[30m\x1b[1m','Dependency check was successful.','\x1b[0m');
     console.log(NAME+' is now accepting requests on port: '+ PORT);
 });
-
-function dependencyCheck(){
-    // The list of required dependencies.
-    var environmentVariableDependencies = ['forwardMxApiKey'];
-    var hasAllDependencies = true;
-
-    // Check for dependencies
-    console.log('\x1b[43m\x1b[30m','Performing dependency check.....','\x1b[0m');
-    for(var i in environmentVariableDependencies){
-        if(!process.env[environmentVariableDependencies[i]]){
-            hasAllDependencies = false;
-            console.log('Missing dependency: \''+environmentVariableDependencies[i]+'\'. Please be sure that the system has an environment variable set for this element.')
-        }
-    }
-    return hasAllDependencies;
-}
 
 module.exports.app = app; //For unit-testing
 module.exports.DOMAIN = DOMAIN;
