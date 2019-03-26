4a36d4f35bb4a691c550bff8d23857ebd8b75b7c
David Stone <dcstone09@gmail.com>
2019-03-26 08:59:50 -0700

    created a folder src to parallel test

---
+++ src/middlewares.js
@@ -0,0 +1,72 @@
+function checkParamsMiddleware(req, res, next) {
+  var requiredParams = ["alias", "realEmail"];
+  var hasRequiredParams = true;
+  for (var i in requiredParams) {
+    // Checks that the API request supplies the required params.
+    if (!req.query[requiredParams[i]]) {
+      hasRequiredParams = false;
+    }
+  }
+  if (hasRequiredParams) {
+    next();
+  } else {
+    res.status(422).send({
+      success: "FALSE",
+      message:
+        "Error: Missing required param. Please ensure that the following params are provided in the query: " +
+        requiredParams
+          .toString()
+          .split(",")
+          .join(", ") +
+        "."
+    });
+  }
+}
+
+function validateParamsMiddleware(req, res, next) {
+  var alias = req.query["alias"];
+  var email = req.query["realEmail"];
+  var emailVerificationRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (alias.includes("@")) {
+    res.status(422).send({
+      success: "FALSE",
+      message:
+        "Error: The supplied alias parameter is invalid. Please be sure that it does not contain an '@' symbol."
+    });
+  } else {
+    if (!emailVerificationRegex.test(email)) {
+      res.status(422).send({
+        success: "FALSE",
+        message: "Error: The supplied realEmail parameter is invalid."
+      });
+    } else {
+      next();
+    }
+  }
+}
+
+function corsMiddleware(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://pseudoname.io");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+}
+
+function loggingMiddleware(req, res, next) {
+  console.log(
+    req.path +
+      " request received from req.ip: " +
+      req.ip +
+      ", req.connection.remoteAddress: " +
+      req.connection.remoteAddress +
+      ", req.secure: " +
+      req.secure
+  );
+  next();
+}
+
+module.exports.corsMiddleware = corsMiddleware;
+module.exports.checkParamsMiddleware = checkParamsMiddleware;
+module.exports.loggingMiddleware = loggingMiddleware;
+module.exports.validateParamsMiddleware = validateParamsMiddleware;
--- test/test.middlewares.js
+++ test/test.middlewares.js
@@ -1,130 +1,159 @@
-var chai = require('chai');
-var mocha = require('mocha');
-var app = require('../index.js').app;
+var chai = require("chai");
+var mocha = require("mocha");
+var app = require("../src").app;
 var expect = chai.expect;
-var chaiHttp = require('chai-http');
-var index = require('../index');
+var chaiHttp = require("chai-http");
+var index = require("../src");
 
 chai.use(chaiHttp);
 
-describe('Middleware Tests:', function() {
-    describe ('checkParamsMiddleware Tests', function(){
-        describe('\'/add/\' Tests:', function () {
-            describe('Trying to add an alias without either param test', function () {
-                it('should be disallowed and the user should be told to provide both params', (done) => {
-                    chai.request('localhost:' + index.PORT)
-                        .get('/add/')
-                        .send()
-                        .end((err, res) => {
-                            expect(res.status).to.equal(422);
-                            expect(res.body).to.have.property('success', 'FALSE');
-                            expect(res.body).to.have.property('message', 'Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail.');
-                            done();
-                        });
-                });
-
-            });
-
-            describe('Trying to add an alias with an alias param, without a realEmail param test', function () {
-                it('should be disallowed and the user should be told to provide both params', (done) => {
-                    chai.request('localhost:' + index.PORT)
-                        .get('/add/?alias=uniqueAlias')
-                        .send()
-                        .end((err, res) => {
-                            expect(res.status).to.equal(422);
-                            expect(res.body).to.have.property('success', 'FALSE');
-                            expect(res.body).to.have.property('message', 'Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail.');
-                            done();
-                        });
-                });
-            });
-
-            describe('Trying to add an alias with a realEmail param, without an alias param test', function () {
-                it('should be disallowed and the user should be told to provide both params', (done) => {
-                    chai.request('localhost:' + index.PORT)
-                        .get('/add/?realEmail=realEmail@gmail.com')
-                        .send()
-                        .end((err, res) => {
-                            expect(res.status).to.deep.equal(422);
-                            expect(res.body).to.have.property('success', 'FALSE');
-                            expect(res.body).to.have.property('message', 'Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail.');
-                            done();
-                        });
-                });
+describe("Middleware Tests:", function() {
+  describe("checkParamsMiddleware Tests", function() {
+    describe("'/add/' Tests:", function() {
+      describe("Trying to add an alias without either param test", function() {
+        it("should be disallowed and the user should be told to provide both params", done => {
+          chai
+            .request("localhost:" + index.PORT)
+            .get("/add/")
+            .send()
+            .end((err, res) => {
+              expect(res.status).to.equal(422);
+              expect(res.body).to.have.property("success", "FALSE");
+              expect(res.body).to.have.property(
+                "message",
+                "Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail."
+              );
+              done();
             });
         });
-        describe('\'/delete/\' Tests', function (){
-            describe('Trying to delete an alias without either param test' ,function(){
-                it('should be disallowed and the user should be told to provide both params',(done) => {
-                    chai.request('localhost:' + index.PORT)
-                        .get('/delete/')
-                        .send()
-                        .end((err, res) => {
-                            expect(res.status).to.equal(422);
-                            expect(res.body).to.have.property('success', 'FALSE');
-                            expect(res.body).to.have.property('message', 'Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail.');
-                            done();
-                        });
-                });
+      });
 
+      describe("Trying to add an alias with an alias param, without a realEmail param test", function() {
+        it("should be disallowed and the user should be told to provide both params", done => {
+          chai
+            .request("localhost:" + index.PORT)
+            .get("/add/?alias=uniqueAlias")
+            .send()
+            .end((err, res) => {
+              expect(res.status).to.equal(422);
+              expect(res.body).to.have.property("success", "FALSE");
+              expect(res.body).to.have.property(
+                "message",
+                "Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail."
+              );
+              done();
             });
-
-            describe('Trying to delete an alias with an alias param, without a realEmail param test' ,function(){
-                it('should be disallowed and the user should be told to provide both params',(done) => {
-                    chai.request('localhost:'+index.PORT)
-                        .get('/delete/?alias=uniqueAlias')
-                        .send()
-                        .end((err, res) => {
-                            expect(res.status).to.equal(422);
-                            expect(res.body).to.have.property('success', 'FALSE');
-                            expect(res.body).to.have.property('message', 'Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail.');
-                            done();
-                        });
-                    });
-            });
-
-            describe('Trying to delete an alias with a realEmail param, without an alias param test' ,function(){
-                it('should be disallowed and the user should be told to provide both params',(done) => {
-                    chai.request('localhost:'+index.PORT)
-                        .get('/delete/?realEmail=test@gmail.com')
-                        .send()
-                        .end((err, res) => {
-                            expect(res.status).to.deep.equal(422);
-                            expect(res.body).to.have.property('success', 'FALSE');
-                            expect(res.body).to.have.property('message', 'Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail.');
-                            done();
-                        });
-                });
-            });
-
         });
+      });
+
+      describe("Trying to add an alias with a realEmail param, without an alias param test", function() {
+        it("should be disallowed and the user should be told to provide both params", done => {
+          chai
+            .request("localhost:" + index.PORT)
+            .get("/add/?realEmail=realEmail@gmail.com")
+            .send()
+            .end((err, res) => {
+              expect(res.status).to.deep.equal(422);
+              expect(res.body).to.have.property("success", "FALSE");
+              expect(res.body).to.have.property(
+                "message",
+                "Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail."
+              );
+              done();
+            });
+        });
+      });
     });
-    describe ('validateParamsMiddleware Tests', function(){
-        describe('Trying to submit a request with an invalid alias' ,function(){
-            it('should be rejected with a message as to why',(done) => {
-                chai.request('localhost:'+index.PORT)
-                    .get('/add/?alias=@alias&realEmail=test@gmail.com')
-                    .send()
-                    .end((err, res) => {
-                        expect(res.status).to.deep.equal(422);
-                        expect(res.body).to.have.property('success', 'FALSE');
-                        expect(res.body).to.have.property('message', 'Error: The supplied alias parameter is invalid. Please be sure that it does not contain an \'@\' symbol.');
-                        done();
-                    });
+    describe("'/delete/' Tests", function() {
+      describe("Trying to delete an alias without either param test", function() {
+        it("should be disallowed and the user should be told to provide both params", done => {
+          chai
+            .request("localhost:" + index.PORT)
+            .get("/delete/")
+            .send()
+            .end((err, res) => {
+              expect(res.status).to.equal(422);
+              expect(res.body).to.have.property("success", "FALSE");
+              expect(res.body).to.have.property(
+                "message",
+                "Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail."
+              );
+              done();
             });
         });
-        describe('Trying to submit a request with an invalid email' ,function(){
-            it('should be rejected with a message as to why',(done) => {
-                chai.request('localhost:'+index.PORT)
-                    .get('/add/?alias=alias111&realEmail=test')
-                    .send()
-                    .end((err, res) => {
-                        expect(res.status).to.deep.equal(422);
-                        expect(res.body).to.have.property('success', 'FALSE');
-                        expect(res.body).to.have.property('message', 'Error: The supplied realEmail parameter is invalid.');
-                        done();
-                    });
+      });
+
+      describe("Trying to delete an alias with an alias param, without a realEmail param test", function() {
+        it("should be disallowed and the user should be told to provide both params", done => {
+          chai
+            .request("localhost:" + index.PORT)
+            .get("/delete/?alias=uniqueAlias")
+            .send()
+            .end((err, res) => {
+              expect(res.status).to.equal(422);
+              expect(res.body).to.have.property("success", "FALSE");
+              expect(res.body).to.have.property(
+                "message",
+                "Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail."
+              );
+              done();
             });
         });
+      });
+
+      describe("Trying to delete an alias with a realEmail param, without an alias param test", function() {
+        it("should be disallowed and the user should be told to provide both params", done => {
+          chai
+            .request("localhost:" + index.PORT)
+            .get("/delete/?realEmail=test@gmail.com")
+            .send()
+            .end((err, res) => {
+              expect(res.status).to.deep.equal(422);
+              expect(res.body).to.have.property("success", "FALSE");
+              expect(res.body).to.have.property(
+                "message",
+                "Error: Missing required param. Please ensure that the following params are provided in the query: alias, realEmail."
+              );
+              done();
+            });
+        });
+      });
     });
+  });
+  describe("validateParamsMiddleware Tests", function() {
+    describe("Trying to submit a request with an invalid alias", function() {
+      it("should be rejected with a message as to why", done => {
+        chai
+          .request("localhost:" + index.PORT)
+          .get("/add/?alias=@alias&realEmail=test@gmail.com")
+          .send()
+          .end((err, res) => {
+            expect(res.status).to.deep.equal(422);
+            expect(res.body).to.have.property("success", "FALSE");
+            expect(res.body).to.have.property(
+              "message",
+              "Error: The supplied alias parameter is invalid. Please be sure that it does not contain an '@' symbol."
+            );
+            done();
+          });
+      });
+    });
+    describe("Trying to submit a request with an invalid email", function() {
+      it("should be rejected with a message as to why", done => {
+        chai
+          .request("localhost:" + index.PORT)
+          .get("/add/?alias=alias111&realEmail=test")
+          .send()
+          .end((err, res) => {
+            expect(res.status).to.deep.equal(422);
+            expect(res.body).to.have.property("success", "FALSE");
+            expect(res.body).to.have.property(
+              "message",
+              "Error: The supplied realEmail parameter is invalid."
+            );
+            done();
+          });
+      });
+    });
+  });
 });
