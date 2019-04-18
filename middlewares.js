c4ee6a2fbe9e46fe74a801c47334a80aaaedb967
ZacharyDavidSaunders <zacharydavidsaunders@gmail.com>
2019-04-17 22:34:02 -0700

    Added ECMAScript 8 / 2017 compliance, tweaking logging middleware
    * Updated ESLint Rules 
    * Updated README

--- middlewares.js
+++ middlewares.js
@@ -1,54 +1,55 @@
+const index = require('./index');
+
 function checkParamsMiddleware(req, res, next) {
-    var requiredParams = ['alias','realEmail'];
-    var hasRequiredParams = true;
-    for(var i in requiredParams){
-        // Checks that the API request supplies the required params.
-        if(!req.query[requiredParams[i]]){
-            hasRequiredParams = false;
-        }
+  const requiredParams = ['alias', 'realEmail'];
+  let hasRequiredParams = true;
+
+  // Checks that the API request supplies the required params.
+  for (let i = 0; i < requiredParams.length; i++) {
+    if (!req.query[requiredParams[i]]) {
+      hasRequiredParams = false;
     }
-    if(hasRequiredParams){
-        next();
-    }else{
-        res.status(422).send({
-            success: 'FALSE',
-            message: 'Error: Missing required param. Please ensure that the following params are provided in the query: '+requiredParams.toString().split(',').join(', ')+'.'
-        });
-    }
+  }
+  if (hasRequiredParams) {
+    next();
+  } else {
+    res.status(422).send({
+      success: 'FALSE',
+      message: `Error: Missing required param. Please ensure that the following params are provided in the query: ${requiredParams.toString().split(',').join(', ')}.`,
+    });
+  }
 }
 
 function validateParamsMiddleware(req, res, next) {
-    var alias = req.query['alias'];
-    var email = req.query['realEmail'];
-    var emailVerificationRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const alias = req.query.alias;
+  const email = req.query.realEmail;
+  const emailVerificationRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(alias.includes('@')){
-        res.status(422).send({
-            success: 'FALSE',
-            message: 'Error: The supplied alias parameter is invalid. Please be sure that it does not contain an \'@\' symbol.'
-        });
-    }else{
-        if(!emailVerificationRegex.test(email)){
-            res.status(422).send({
-                success: 'FALSE',
-                message: 'Error: The supplied realEmail parameter is invalid.'
-            });
-        }else{
-            next();
-        }
-    }
+  if (alias.includes('@')) {
+    res.status(422).send({
+      success: 'FALSE',
+      message: 'Error: The supplied alias parameter is invalid. Please be sure that it does not contain an \'@\' symbol.',
+    });
+  } else if (!emailVerificationRegex.test(email)) {
+    res.status(422).send({
+      success: 'FALSE',
+      message: 'Error: The supplied realEmail parameter is invalid.',
+    });
+  } else {
+    next();
+  }
 }
 
 function corsMiddleware(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://pseudoname.io');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+  res.header('Access-Control-Allow-Origin', index.DOMAIN);
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 }
 
-function loggingMiddleware(req, res, next){
-    console.log(req.path+' request received from req.ip: ' + req.ip+ ', req.connection.remoteAddress: '+req.connection.remoteAddress+', req.secure: '+req.secure);
-    next();
+function loggingMiddleware(req, res, next) {
+  console.log(`${req.method} request to '${req.path}' received from req.ip: ${req.ip}, req.secure: ${req.secure}`);
+  next();
 }
 
 module.exports.corsMiddleware = corsMiddleware;
--- test/test.routes.js
+++ test/test.routes.js
@@ -1,132 +1,133 @@
-var chai = require('chai');
-var mocha = require('mocha');
-var app = require('../index.js').app;
-var expect = chai.expect;
-var chaiHttp = require('chai-http');
-var index = require('../index');
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+// These linting exceptions allow the tests to better match the syntax of the documentation.
 
+const chai = require('chai');
+const mocha = require('mocha');
+const chaiHttp = require('chai-http');
+const index = require('../index');
+
+const expect = chai.expect;
+const describe = mocha.describe;
+const it = mocha.it;
 chai.use(chaiHttp);
 
-describe('Route Tests:', function() {
-    describe('Unsupported Routes Tests:', function(){
-        describe('\'/unusedRoute/\' test' ,function() {
-            it('should respond with a 404', (done) => {
-                chai.request('localhost:'+index.PORT)
-                    .get('/unusedRoute/')
-                    .send()
-                    .end((err, res) => {
-                        expect(res.status).to.equal(404);
-                        done();
-                    });
-            });
-        });
-        describe('\'/edit/\' test' ,function() {
-            it('should respond with a 404', (done) => {
-                chai.request('localhost:'+index.PORT)
-                    .get('/edit/')
-                    .send()
-                    .end((err, res) => {
-                        expect(res.status).to.equal(404);
-                        done();
-                    });
-            });
-        });
-        describe('\'/somethingElse/\' test' ,function() {
-            it('should respond with a 404', (done) => {
-                chai.request('localhost:'+index.PORT)
-                    .get('/somethingElse/')
-                    .send()
-                    .end((err, res) => {
-                        expect(res.status).to.equal(404);
-                        done();
-                    });
-            });
+describe('Route Tests:', function () {
+  describe('Unsupported Routes Tests:', function () {
+    describe('\'/unusedRoute/\' test', function () {
+      it('should respond with a 404', (done) => {
+        chai.request(`localhost:${index.PORT}`)
+          .get('/unusedRoute/')
+          .send()
+          .end((err, res) => {
+            expect(res.status).to.equal(404);
+            done();
+          });
+      });
+    });
+    describe('\'/edit/\' test', function () {
+      it('should respond with a 404', (done) => {
+        chai.request(`localhost:${index.PORT}`)
+          .get('/edit/')
+          .send()
+          .end((err, res) => {
+            expect(res.status).to.equal(404);
+            done();
+          });
+      });
+    });
+    describe('\'/somethingElse/\' test', function () {
+      it('should respond with a 404', (done) => {
+        chai.request(`localhost:${index.PORT}`)
+          .get('/somethingElse/')
+          .send()
+          .end((err, res) => {
+            expect(res.status).to.equal(404);
+            done();
+          });
+      });
+    });
+  });
+  describe('\'/\' test', function () {
+    it('Should confirm connection, display the API name, version, and documentation URL.', (done) => {
+      chai.request(`localhost:${index.PORT}`)
+        .get('/')
+        .send()
+        .end((err, res) => {
+          expect(res.status).to.deep.equal(200);
+          expect(res.body).to.have.property('success');
+          expect(res.body.success).to.deep.equal('TRUE');
+          expect(res.body).to.have.property('message');
+          expect(res.body.message).to.deep.equal(`You have reached ${index.NAME}v${index.VERSION}. Please see the API Documentation for more information: ${index.DOCUMENTATION}`);
+          done();
         });
     });
-    describe('\'/\' test' ,function() {
-        it('Should confirm connection, display the API name, version, and documentation URL.', (done) => {
-            chai.request('localhost:'+index.PORT)
-                .get('/')
-                .send()
-                .end((err, res) => {
-                  expect(res.status).to.deep.equal(200);
-                  expect(res.body).to.have.property('success');
-                  expect(res.body.success).to.deep.equal('TRUE');
-                  expect(res.body).to.have.property('message');
-                  expect(res.body.message).to.deep.equal('You have reached '+index.NAME+'v'+index.VERSION+'. Please see the API Documentation for more information: '+index.DOCUMENTATION);
-                  done();
-                });
-        });
-    });
-    describe('\'/add/\' Tests:', function () {
-        describe('Trying to add an alias correctly' ,function(){
-            it('should either be added or say that the alias is already taken',(done) => {
-                chai.request('localhost:'+index.PORT)
-                    .get('/add/?alias=newAlias&realEmail=test@gmail.com')
-                    .send()
-                    .end((err, res) => {
-                        expect(res.status).to.deep.equal(200);
-                        expect(res.body).to.have.property('success');
-                        expect(res.body.success).to.satisfy(function (success) {
-                            if ((success === 'TRUE') || (success === 'FALSE')) {
-                                return true;
-                            } else {
-                                return false;
-                            }
-                        });
-                        expect(res.body).to.have.property('message');
-                        expect(res.body.message).to.satisfy(function (message) {
-                            if ((message === 'Alias has been created. Please wait 60 seconds before sending emails to the alias. Doing so ensures that the all systems have been updated and emails are not lost.') || (message === 'Error: Duplicate alias request refused.')) {
-                                return true;
-                            } else {
-                                return false;
-                            }
-                        });
-                        done();
-                    });
+  });
+  describe('\'/add/\' Tests:', function () {
+    describe('Trying to add an alias correctly', function () {
+      it('should either be added or say that the alias is already taken', (done) => {
+        chai.request(`localhost:${index.PORT}`)
+          .get('/add/?alias=newAlias&realEmail=test@gmail.com')
+          .send()
+          .end((err, res) => {
+            expect(res.status).to.deep.equal(200);
+            expect(res.body).to.have.property('success');
+            expect(res.body.success).to.satisfy(function (success) {
+              if ((success === 'TRUE') || (success === 'FALSE')) {
+                return true;
+              }
+              return false;
             });
-        });
-    });
-    describe('\'/delete/\' Tests:', function () {
-        describe('Trying to delete an alias correctly' ,function(){
-            it('should either be deleted or say that the alias does not yet exist',(done) => {
-                chai.request('localhost:'+index.PORT)
-                    .get('/delete/?alias=newAlias&realEmail=test@gmail.com')
-                    .send()
-                    .end((err, res) => {
-                        expect(res.status).to.deep.equal(200);
-                        expect(res.body).to.have.property('success');
-                        expect(res.body.success).to.satisfy(function (success) {
-                            if ((success === 'TRUE') || (success === 'FALSE')) {
-                                return true;
-                            } else {
-                                return false;
-                            }
-                        });
-                        expect(res.body).to.have.property('message');
-                        expect(res.body.message).to.satisfy(function (message) {
-                            if ((message === 'Alias has been deleted.') || (message === 'Error: Alias has not yet been registered and thus may not be deleted.')) {
-                                return true;
-                            } else {
-                                return false;
-                            }
-                        });
-                        done();
-                    });
+            expect(res.body).to.have.property('message');
+            expect(res.body.message).to.satisfy(function (message) {
+              if ((message === 'Alias has been created. Please wait 60 seconds before sending emails to the alias. Doing so ensures that the all systems have been updated and emails are not lost.') || (message === 'Error: Duplicate alias request refused.')) {
+                return true;
+              }
+              return false;
             });
-        });
-        describe('Trying to delete an alias not that the provided email does not own' ,function(){
-            it('should be refused with an explanation as to why',(done) => {
-                chai.request('localhost:'+index.PORT)
-                    .get('/delete/?alias=contactus&realEmail=test@gmail.com')
-                    .send()
-                    .end((err, res) => {
-                        expect(res.status).to.equal(200);
-                        expect(res.body).to.have.property('success', 'FALSE');
-                        expect(res.body).to.have.property('message', 'Error: Deletion denied. The provided alias is not owned by the provided email.');
-                        done();
-                    });
-            });
-        });
+            done();
+          });
+      });
     });
+  });
+  describe('\'/delete/\' Tests:', function () {
+    describe('Trying to delete an alias correctly', function () {
+      it('should either be deleted or say that the alias does not yet exist', (done) => {
+        chai.request(`localhost:${index.PORT}`)
+          .get('/delete/?alias=newAlias&realEmail=test@gmail.com')
+          .send()
+          .end((err, res) => {
+            expect(res.status).to.deep.equal(200);
+            expect(res.body).to.have.property('success');
+            expect(res.body.success).to.satisfy(function (success) {
+              if ((success === 'TRUE') || (success === 'FALSE')) {
+                return true;
+              }
+              return false;
+            });
+            expect(res.body).to.have.property('message');
+            expect(res.body.message).to.satisfy(function (message) {
+              if ((message === 'Alias has been deleted.') || (message === 'Error: Alias has not yet been registered and thus may not be deleted.')) {
+                return true;
+              }
+              return false;
+            });
+            done();
+          });
+      });
+    });
+    describe('Trying to delete an alias not that the provided email does not own', function () {
+      it('should be refused with an explanation as to why', (done) => {
+        chai.request(`localhost:${index.PORT}`)
+          .get('/delete/?alias=contactus&realEmail=test@gmail.com')
+          .send()
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property('success', 'FALSE');
+            expect(res.body).to.have.property('message', 'Error: Deletion denied. The provided alias is not owned by the provided email.');
+            done();
+          });
+      });
+    });
+  });
 });
